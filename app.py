000e9da1faf1697c1f05b09511c260f98f228db1
amitaryan0010 <amitaryan0010@gmail.com>
2025-01-18 16:32:16 +0530

    updated file

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 
 from flask import flask
 app = Flask(__name__)
+
+@app.rount("/"
 def lw():
     return"Welcome to Sunday demo"
 
-app.ruun(host=;0.0.0.0', port=5000)
+app.ruun(host='0.0.0.0', port=5000)
