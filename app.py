847e587abf906874c40496e629803348d2daf44f
amitaryan0010 <amitaryan0010@gmail.com>
2025-01-18 16:35:04 +0530

    updated file

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def lw():
-    return"Welcome to Sunday demo"
+    return "Welcome to Sunday demo"
 
-app.ruun(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=5000)
